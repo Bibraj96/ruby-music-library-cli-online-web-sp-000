c5c376aa4bf8379c7725288732495dbc1da249ed
Blend Ibraj <blendi.ibraj96@gmail.com>
2020-04-13 16:40:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,10 +39,12 @@ class Song
     genre.songs << self unless genre.songs.include?(self)
   end
 
-  def find_by_name
+  def self.find_by_name(name)
+    all.detect {|n| n.name == name}
   end
 
-  def find_or_create_by_name
+  def self.find_or_create_by_name(name)
+    find_by_name(name) || create(name)
   end
 
 end
