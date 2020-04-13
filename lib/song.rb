50c6a474dc470bce3a40253897bafa2781e14756
Blend Ibraj <blendi.ibraj96@gmail.com>
2020-04-13 17:00:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,7 +46,7 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    if self.find_by_name(name) == false
+    if self.find_by_name(name) == nil
       self.create(name)
     else
       self.find_by_name(name)
