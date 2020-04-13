ae95c95c4964e61f2acbd788153b5105e22c0973
Blend Ibraj <blendi.ibraj96@gmail.com>
2020-04-13 16:21:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,11 +34,8 @@ class Song
     artist.add_song(self)
   end
 
-  def genre(genre)
-    @genre = genre
-  end
-
   def genre=(genre)
+    @genre = genre
     genre.songs << self unless genre.songs.include?(self)
   end
 
